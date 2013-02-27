d7ad9b0311328241ab2a10b1ebd083e74244393b
Alexandr <godetskiy@gmail.com>
2013-02-27 21:36:34 +0200

    HelloSpring...

--- HelloSpring/src/main/java/HelloSpring/controller/HomeController.java
+++ HelloSpring/src/main/java/HelloSpring/controller/HomeController.java
@@ -1,26 +1,19 @@
-package HelloSpring.controllers;
+package HelloSpring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 @Controller
-@RequestMapping("/")
-public class HelloController {
-
-    @RequestMapping(method = RequestMethod.GET)
-    public String printWelcome(ModelMap model) {
-
+public class HomeController {
+	@RequestMapping(value="/")
+    public String test(ModelMap model) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy hh:mm");
         String str = sdf.format(new Date());
-
-        model.addAttribute("dateTime", str);
-        return "index";
-
+        model.addAttribute("datetime", str);
+        return "home";
     }
-
 }
--- HelloSpring/src/main/webapp/WEB-INF/index.jsp
+++
@@ -1,9 +0,0 @@
-<html>
-<header>
-    <title>Главное окно</title>
-</header>
-<body>
-    <h1>Hello World! : ${dateTime}</h1>
-</body>
-
-</html>
